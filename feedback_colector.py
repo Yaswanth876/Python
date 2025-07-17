5af3551f0dc207d770aace61ef124b539bd58a04
Yaswanth876 <vsyaswanth008@gmail.com>
2025-07-17 23:40:44 +0530

    added

---
+++ feedback_colector.py
@@ -0,0 +1,16 @@
+name = input("Enter your name: ")
+email = input("Enter your E-mail: ")
+feedback = input("Enter your feedback: ")
+
+# Writing formatted feedback to file
+with open("user_feedback.txt", "w") as f:
+    f.write("Name     : " + name + "\n")
+    f.write("Email    : " + email + "\n")
+    f.write("Feedback : " + feedback + "\n")
+
+# Reading from file
+with open("user_feedback.txt", "r") as f:
+    user_feedback = f.read()
+
+print("\n📝 User Feedback Received:\n--------------------------")
+print(user_feedback)
